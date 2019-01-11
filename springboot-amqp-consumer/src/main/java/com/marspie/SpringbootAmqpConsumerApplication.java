2ad5ac4d622aadd6e4cdfa16b97bf9fc4c231734
yaonew <yaonew@126.com>
2019-01-11 09:46:00 +0800

    springboot demo

---
+++ springboot-amqp-consumer/src/main/java/com/marspie/SpringbootAmqpConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.marspie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAmqpConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAmqpConsumerApplication.class, args);
+    }
+
+}
+
---
+++ springboot-amqp-producer/src/main/java/com/marspie/SpringbootAmqpProducerApplication.java
@@ -0,0 +1,14 @@
+package com.marspie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAmqpProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAmqpProducerApplication.class, args);
+    }
+
+}
+
